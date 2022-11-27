cd80bd9bbab467a968e603c3bbf8c84f2b9945ca
lollerfirst <lollerfirst@gmail.com>
2022-11-27 22:34:39 +0100

    Lorem Ipsum

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lollerfirst/dev/Compiler-Project
+# Build directory: /home/lollerfirst/dev/Compiler-Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ build/src/CMakeFiles/libcompiler.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "liblibcompiler.a"
+)
